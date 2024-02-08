b0353a3babb56dd31fcbec5aeb2b03c613664e1b
xwhoknowz <jeremiah2498@gmail.com>
2024-02-08 13:14:36 -0600

    tweaked started code

---
+++ models/lists.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Lists extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    run_time: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    poster: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'list',
+  }
+);
+
+module.exports = Lists;
