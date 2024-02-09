93c3f0037c2ed7ff3afe2ebdec7c8e6d823a98ad
xwhoknowz <jeremiah2498@gmail.com>
2024-02-09 13:41:52 -0600

    wrote /login ep for homeRoutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { List, Movie, Review, User } = require('../models');
+const withAuth = require('../utils/withAuth')
 const isAdmin = require('../utils/isAdmin');
 
 router.get (`/`, async (req, res) =>{
@@ -26,8 +27,23 @@ try {
 
 router.get(`/admin`, [withAuth, isAdmin], async (req, res) =>{
     try {
+
+        res.render(`admin`, {
+
         
+        });
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
+
+router.get(`/login`, (req, res) => {
+    if (req.session.logged_in){
+        res.redirect(`/homepage`)
+        return;
+    } else {
+        res.render(`login`);
+    };
+});
+
+module.exports = router;
