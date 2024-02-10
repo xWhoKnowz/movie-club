b161017c2b099be86a09d5a06d3de5378939a9ca
xwhoknowz <jeremiah2498@gmail.com>
2024-02-09 19:11:55 -0600

    working on the profile page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -9,7 +9,7 @@ try {
         include: [
             {
                 model: Movie,
-                attributes: [title, poster, rating]
+                attributes: [title, poster, rating, run_time]
             },
         ],
     });
@@ -44,6 +44,50 @@ router.get(`/login`, (req, res) => {
     } else {
         res.render(`login`);
     };
+});
+
+router.get(`movies`, withAuth, async (req, res) => {
+    try {
+        
+        const movieData = await Movie.findByPk(req.params.id, {
+            include:[
+                {
+                    model: Review,
+                    attributes: [title, body, rating]
+                },
+                {
+                    model:Comment,
+                    attributes:[body]
+                },
+                {
+                    model: User,
+                    attributes: username
+                }
+            ]
+        });
+
+        const movieList = movieData.get({ plain: true })
+
+        res.render(`homepage`, {
+            movieList,
+            logged_in: req.session.logged_in
+        });
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+router.get(`user`, withAuth, async (req, res) => {
+    try {
+        const userData = User.findByPk(req.session.user_id, {
+            
+        })
+        
+    } catch (error) {
+        
+    }
 });
 
 module.exports = router;
